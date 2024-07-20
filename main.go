d975150223a94efab87d6ce145f9f80a95e9b02f
lucas vinicius <lucassilved@gmail.com>
2024-07-20 13:26:24 -0300

    feat: add logger in main

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "github.com/lucassf2k/gopportunities-back/router"
+import (
+	"github.com/lucassf2k/gopportunities-back/config"
+	"github.com/lucassf2k/gopportunities-back/router"
+)
+
+var (
+	logger *config.Logger
+)
 
 func main() {
+	logger = config.GetLogger("main")
+	err := config.Init()
+	if err != nil {
+		logger.Errf("config initialization error: %v", err)
+		return
+	}
 	router.Initialize()
 }
--- router/router.go
+++ router/router.go
@@ -3,11 +3,7 @@ package router
 import "github.com/gin-gonic/gin"
 
 func Initialize() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run(":8080")
+	router := gin.Default()
+	initializeRoutes(router)
+	router.Run(":8080")
 }
