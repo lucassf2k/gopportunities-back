da4ee982d5443a04c5acb9528d7fd65880a62da4
lucas vinicius <lucassilved@gmail.com>
2024-07-21 20:11:00 -0300

    feat: implementation listOpenings

--- handler/listOpening.go
+++ handler/listOpening.go
@@ -4,10 +4,14 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lucassf2k/gopportunities-back/schemas"
 )
 
 func ListOpeningsHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET Opening",
-	})
+	openings := []schemas.Opening{}
+	if err := db.Find(&openings).Error; err != nil {
+		sendError(ctx, http.StatusInternalServerError, "error listing openings")
+		return
+	}
+	sendSucess(ctx, "list-openings", openings)
 }
