9ce7446c9716f6e741a6fe4ac7364166852c86fe
lucas vinicius <lucassilved@gmail.com>
2024-07-23 10:27:30 -0300

    feat: implementing updateOpening

--- handler/request.go
+++ handler/request.go
@@ -15,27 +15,43 @@ type CreateOpeningRequest struct {
 	Salary   int64  `json:"salary"`
 }
 
-func (cpr *CreateOpeningRequest) Validate() error {
-	if cpr.Role == "" && cpr.Company == "" && cpr.Location == "" && cpr.Link == "" && cpr.Remote == nil && cpr.Salary <= 0 {
+func (cor *CreateOpeningRequest) Validate() error {
+	if cor.Role == "" && cor.Company == "" && cor.Location == "" && cor.Link == "" && cor.Remote == nil && cor.Salary <= 0 {
 		return fmt.Errorf("request body is empty or malformed")
 	}
-	if cpr.Role == "" {
+	if cor.Role == "" {
 		return errParamsIsRequired("role", "string")
 	}
-	if cpr.Company == "" {
+	if cor.Company == "" {
 		return errParamsIsRequired("company", "string")
 	}
-	if cpr.Location == "" {
+	if cor.Location == "" {
 		return errParamsIsRequired("location", "string")
 	}
-	if cpr.Link == "" {
+	if cor.Link == "" {
 		return errParamsIsRequired("link", "string")
 	}
-	if cpr.Remote == nil {
+	if cor.Remote == nil {
 		return errParamsIsRequired("remote", "bool")
 	}
-	if cpr.Salary <= 0 {
+	if cor.Salary <= 0 {
 		return errParamsIsRequired("salary", "int64")
 	}
 	return nil
+}
+
+type UpdateOpeningRequest struct {
+	Role     string `json:"role"`
+	Company  string `json:"company"`
+	Location string `json:"location"`
+	Remote   *bool  `json:"remote"`
+	Link     string `json:"link"`
+	Salary   int64  `json:"salary"`
+}
+
+func (uor *UpdateOpeningRequest) Validate() error {
+	if uor.Role != "" || uor.Company != "" || uor.Link != "" || uor.Location != "" || uor.Remote != nil || uor.Salary > 0 {
+		return nil
+	}
+	return fmt.Errorf("at least one valid field must be provided")
 }
--- handler/updateOpening.go
+++ handler/updateOpening.go
@@ -4,10 +4,49 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/lucassf2k/gopportunities-back/schemas"
 )
 
 func UpdateOpeningHandler(ctx *gin.Context) {
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": "GET Opening",
-	})
+	request := UpdateOpeningRequest{}
+	ctx.BindJSON(&request)
+	if err := request.Validate(); err != nil {
+		logger.Errf("validation error: %v", err.Error())
+		sendError(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+	id := ctx.Query("id")
+	if id != "" {
+		sendError(ctx, http.StatusBadRequest, errParamsIsRequired("id", "queryParameter").Error())
+		return
+	}
+	opening := schemas.Opening{}
+	if err := db.First(&opening, id).Error; err != nil {
+		sendError(ctx, http.StatusNotFound, "opening not found")
+		return
+	}
+	if request.Company != "" {
+		opening.Company = request.Company
+	}
+	if request.Role != "" {
+		opening.Role = request.Role
+	}
+	if request.Link != "" {
+		opening.Link = request.Link
+	}
+	if request.Location != "" {
+		opening.Location = request.Location
+	}
+	if request.Remote != nil {
+		opening.Remote = *request.Remote
+	}
+	if request.Salary > 0 {
+		opening.Salary = request.Salary
+	}
+	if err := db.Save(&opening).Error; err != nil {
+		logger.Errf("error updating opening: %v", err.Error())
+		sendError(ctx, http.StatusInternalServerError, "error updating opening")
+		return
+	}
+	sendSucess(ctx, "updating-opening", opening)
 }
